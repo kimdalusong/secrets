4cab5aa0ff532348d57e078ad53069cf8be9a7ba
kimdalusong <kimdalusong05@gmail.com>
2020-04-26 17:16:54 +0800

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -15,10 +17,14 @@ app.use(bodyParser.urlencoded({
 }));
 
 //Schema
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
+
 
 //Model
 const User = mongoose.model("User", userSchema);
